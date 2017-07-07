47766c415e5669305a6c789e0a925423c71da43b
nathanye <khalil54781@gmail.com>
2017-07-07 19:47:47 +0800

    user can't see the page what is hidden in the job index

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,7 +3,7 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 
   def index
-    @jobs = Job.all
+    @jobs = Job.where(:is_hidden => false).recent
   end
 
   def new
