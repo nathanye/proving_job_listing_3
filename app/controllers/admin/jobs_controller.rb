209008d7b5f055c550614b3a8a404347028e5871
nathanye <khalil54781@gmail.com>
2017-07-07 18:32:08 +0800

    add is_admin

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,7 @@
 class Admin::JobsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :require_is_admin
 
   def index
     @jobs = Job.all
